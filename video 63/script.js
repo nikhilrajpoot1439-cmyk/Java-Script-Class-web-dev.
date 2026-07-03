aee6e6e859fd126e0a88d908bf04d7a6bd07ba2a
nikhil <nikhil@kali.kali.com>
2026-07-03 13:48:01 -0400

    Video 63

---
+++ video 63/script.js
@@ -0,0 +1,15 @@
+let a = [1, 2, 3, 4, 5];
+
+console.log(a);
+// console.log(a.length);
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+
+// conssole.log(typeof a);
+// console.log(a. toString());
+// console.log(a.join(" and"));
+
+console.log(a.pop());
+console.log(a.push("Nikhil"));
